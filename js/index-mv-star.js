ab794476307d04bfa468fbd12b835f241e9467c5
mmmurphy <mark@m-murphy.com>
2018-01-15 19:25:46 -0500

    Map displays markers.  Next step to tweak locations and add additional custom options.

--- js/index-mv-star.js
+++ js/index-mv-star.js
@@ -4,19 +4,23 @@
 *     variables and constants go here
 * ***********************************************/
 var data = {
-     mapMarkersJSON: '[{"title": "Pandoras Pies", "position": "{\'lat\': \'36.1014897\', \'lng\': \'-79.50681\'}"}, {"title": "The Root", "position": "{\'lat\': \'36.1007668\', \'lng\': \'-79.5073888\'}"}, {"title": "Elon Fountain", "position": "{\'lat\': \'36.0823893\', \'lng\': \'-79.5224609\'}"}, {"title": "markerCoffee", "position": "{\'lat\': \'36.1051821\', \'lng\': \'-79.5051737\'}"}, {"title": "Steve Wosniak", "position": "{\'lat\': \'36.1038291\', \'lng\': \'-79.5082237\'}"}]',
+     mapMarkersJSON: '[' +
+          '{"title": "Pandoras Pies", "position": {"lat": 36.1014897, "lng": -79.50681}}, ' +
+          '{"title": "The Root", "position": {"lat": 36.1007668, "lng": -79.5073888}}, ' +
+          '{"title": "Elon Fountain", "position": {"lat": 36.1031529, "lng": -79.5049359}}, ' +
+          '{"title": "markerCoffee", "position": {"lat": 36.104145, "lng": -79.5059}}, ' +
+          '{"title": "Steve Wosniak", "position": {"lat": 36.102726, "lng": -79.504673}}]',
           //     {"title": "", "position": "{"lat": "", "lng": ""}},
-     mapMarkersKO: ko.utils.parseJson(this.mapMarkersJSON),
 
 
      mapCenter: {lat: 36.1013906, lng: -79.5067275},
      mapDefaultZoom: 16,
 
 
-     mappedMarkers: ko.utils.arrayMap(this.mapMarkersKO, function(mapMarker) {
-          console.log('data.mappedMarkers');
-          return new Marker(mapMarker);
-     }),
+//     mappedMarkers: ko.utils.arrayMap(this.mapMarkersKO, function(mapMarker) {
+//          console.log('data.mappedMarkers');
+//          return new Marker(mapMarker);
+//     }),
 
      pageHeader: {
           main: 'Elm Street',
@@ -53,7 +57,25 @@ var controller = {
           });
 
           // add markers to map
+          this.displayMarkers(map, data.mapMarkersJSON);
+     },
 
+     displayMarkers: function(mapRef, jsonMarkers) {
+          console.log(jsonMarkers);
+          var arrMarkers = ko.utils.parseJson(jsonMarkers);
+          console.log ('title 1 = ' + arrMarkers[0].title);
+          var loopStop = arrMarkers.length;
+          console.log('lat = ' + arrMarkers[0].position);
+
+          for (loop = 0; loop < loopStop; loop++) {
+               console.log('position for marker ' + loop + ' is :  ' + arrMarkers[loop].position);
+
+               this.marker = new google.maps.Marker({
+                    position: arrMarkers[loop].position,
+                    map: mapRef,
+                    title: arrMarkers[loop].title
+               });
+          }
      },
 
      mapMarker: function (title, position) {
