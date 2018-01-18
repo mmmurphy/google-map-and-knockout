56e0612ee187d03624d981c94180c2ecdb4ed477
mmmurphy <mark@m-murphy.com>
2018-01-18 15:52:33 -0500

    filter buttons work

--- js/index-mv-star.js
+++ js/index-mv-star.js
@@ -1,16 +1,17 @@
-var map;
+
 
 /* **********************************************
 *  Model
 *     variables and constants go here
 * ***********************************************/
 var data = {
+     map: null,
      // data for individual markers
      //     {"title": "", "position": "{"lat": "", "lng": ""}},
      mapMarkersJSON: '[' +
           '{"title": "Pandoras Pies", "position": {"lat": 36.1014897, "lng": -79.50681}, "type": "Resturaunt"}, ' +
           '{"title": "The Root", "position": {"lat": 36.1007668, "lng": -79.5073888}, "type": "Resturaunt"}, ' +
-          '{"title": "Elon Fountain", "position": {"lat": 36.1031529, "lng": -79.5049359}, "type": "Landmark"}, ' +
+          '{"title": "Fonville Fountain", "position": {"lat": 36.1022904, "lng": -79.5049627}, "type": "Landmark"}, ' +
           '{"title": "markerCoffee", "position": {"lat": 36.104145, "lng": -79.5059}, "type": "Resturaunt"}, ' +
           '{"title": "Steve Wosniak", "position": {"lat": 36.102726, "lng": -79.504673}, "type": "Event"}]',
 
@@ -40,12 +41,12 @@ var view = {
      },
 
      displayFilterControls: function() {
-          document.getElementById('filterControls').innerHTML = '' +
-          '<input onclick="controller.init();" type=button value="Display All Markers">' +
-          '<input onclick="controller.init();" type=button value="Display Resturaunts">' +
-          '<input onclick="controller.init();" type=button value="Display Event Areas">' +
-          '<input onclick="controller.init();" type=button value="Display Landmarks">' +
-          '<input onclick="controller.init();" type=button value="Hide Markers">'
+          document.getElementById('floating-panel').innerHTML = '' +
+          '<input onclick="controller.filterMarkers(\'All\');" type="button" value="Display All Markers">' +
+          '<input onclick="controller.filterMarkers(\'Resturaunt\');" type="button" value="Display Resturaunts">' +
+          '<input onclick="controller.filterMarkers(\'Event\');" type="button" value="Display Event Areas">' +
+          '<input onclick="controller.filterMarkers(\'Landmark\');" type="button" value="Display Landmarks">' +
+          '<input onclick="controller.filterMarkers();" type="button" value="Hide Markers">'
      }
 };
 
@@ -60,60 +61,57 @@ var controller = {
           view.header(data.pageHeader.main, data.pageHeader.sub);
 
           // display map area
-          map = new google.maps.Map(document.getElementById('map'), {
-               center: data.mapCenter,
-               zoom: data.mapDefaultZoom
+          data.map = new google.maps.Map(document.getElementById('map'), {
+                         center: data.mapCenter,
+                         zoom: data.mapDefaultZoom,
+                         mapTypeId: 'terrain'
           });
 
           // add markers to map
-          this.initMarkers(map, data.mapMarkersJSON, "All");
+          this.initMarkers(data.map, data.mapMarkersJSON);
 
           // add filter displayButtons
           view.displayFilterControls();
-
-          this.filterMarkers("Resturaunt")
      },
 
      //initMarkers parses the jsonMarkers string and creates each marker
-     initMarkers: function(mapRef, jsonMarkers, filter) {
-//          console.log(jsonMarkers);
+     initMarkers: function(mapRef, jsonMarkers) {
+          // parse marker string to JSON format using Knockout API
           var arrMarkers = ko.utils.parseJson(jsonMarkers);
-//          console.log ('title 1 = ' + arrMarkers[0].title);
           var loopStop = arrMarkers.length;
-//          console.log('lat = ' + arrMarkers[0].position);
-          var visible = true;
 
+          data.activeMarkers = [];
           // loop through the marksers and create a new marker for each one
           for (loop = 0; loop < loopStop; loop++) {
-               console.log('position for marker ' + loop + ' is :  ' + arrMarkers[loop].position.lat);
+//               console.log('position for marker ' + loop + ' is :  ' + arrMarkers[loop].position.lat);
                //add marker object to activeMarkers array
                this.marker = new google.maps.Marker({
                     position: arrMarkers[loop].position,
-                    map: mapRef,
+                    map: data.map,
                     title: arrMarkers[loop].title,
-                    visible:  visible
+                    visible:  true,
+                    animation: google.maps.Animation.DROP
                });
                // add marker to marker object array
                data.activeMarkers.push(this.marker);
           }
      },
-     // filter Markers from display using the marker.visible property
+
+     // use filter string to hide mMarkers from map
+     //     setMap(null) to hide
      filterMarkers: function(filter) {
           var loopStop = data.activeMarkers.length;
           var arrMarkers = ko.utils.parseJson(data.mapMarkersJSON);
+          console.log('filter = ' + filter + '  array length = ' + loopStop);
           for (var loop = 0; loop < loopStop; loop++) {
                // set visible to true if marker.type matches filter
                if (arrMarkers[loop].type == filter || filter == "All") {
-                    data.activeMarkers[loop].visible = true;
+                    console.log("display " + data.activeMarkers[loop].title + " was set to " + data.activeMarkers[loop].visible);
+                    data.activeMarkers[loop].setMap(data.map);
                } else {
-                    data.activeMarkers[loop].visible = false;
+                    console.log("hide " + data.activeMarkers[loop].title + " was set to " + data.activeMarkers[loop].visible);
+                    data.activeMarkers[loop].setMap(null);
                }
-               //activeMarkers[loop].setMap(filter);
           }
-     },
-
-
-     hideMarkers: function() {
-          filterMarkers(null);
      }
 };
